f586e55c75a6586671c44640284ac197a73fd720
OmarCharlesGedeon <charles@effortlesshost.com>
2019-02-04 14:12:25 -0800

    Updates

---
+++ precache-manifest.99cd79e1fb7fc98d26e7be41774b6f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07818551a3393a0c1374",
+    "url": "/ClickClack/static/css/main.815cd2a0.chunk.css"
+  },
+  {
+    "revision": "07818551a3393a0c1374",
+    "url": "/ClickClack/static/js/main.07818551.chunk.js"
+  },
+  {
+    "revision": "4cba89d844917e00e2ab",
+    "url": "/ClickClack/static/js/1.4cba89d8.chunk.js"
+  },
+  {
+    "revision": "f96ed2437f9548dd84bb",
+    "url": "/ClickClack/static/js/runtime~main.f96ed243.js"
+  },
+  {
+    "revision": "a8c0ef7f5f057c8adb62cf701c304efe",
+    "url": "/ClickClack/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ClickClack/precache-manifest.99cd79e1fb7fc98d26e7be41774b6f22.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ClickClack/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
